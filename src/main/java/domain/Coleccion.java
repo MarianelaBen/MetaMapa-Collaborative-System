68f34928d979e987ca73336170ae929a2720f13f
MarianelaBen <mbenitez07@frba.utn.edu.ar>
2025-04-15 23:51:05 -0300

    clase Coleccion creada

---
+++ src/main/java/domain/Coleccion.java
@@ -0,0 +1,24 @@
+package domain;
+
+import domain.fuentes.Fuente;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+
+
+public class Coleccion {
+  @Getter private String titulo;
+  @Getter private String descripcion;
+  @Getter private Fuente fuente;
+  @Setter @Getter private Set<Criterio> criterios;
+  @Getter private Set<Hecho> hechosDeLaColeccion;
+
+
+  public Coleccion(String titulo, String descripcion, Fuente fuente){
+    this.titulo = titulo;
+    this.descripcion = descripcion;
+    this.fuente = fuente;
+    this.hechosDeLaColeccion = new HashSet<>();
+  }
+}
