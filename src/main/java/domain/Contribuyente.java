654e0aaf210956e55f3f7a313f749a3ac99bf7e6
MarianelaBen <mbenitez07@frba.utn.edu.ar>
2025-04-16 00:27:45 -0300

    clase Contribuyente agregada

---
+++ src/main/java/domain/Contribuyente.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class Contribuyente {
+  private String nombre;
+  private String apellido;
+  private Integer edad;
+
+  public Contribuyente(String nombre, Integer edad, String apellido) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.apellido = apellido;
+  }
+
+}
