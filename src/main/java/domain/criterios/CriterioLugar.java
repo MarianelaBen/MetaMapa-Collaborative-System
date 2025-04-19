49109831d8669b5a19a19df58d8e5c41b89ebfd7
KiaraDVecchio <164945102+KiaraDVecchio@users.noreply.github.com>
2025-04-19 19:30:12 -0300

    Agrego CriterioLugar junto con su test y pasa

--- src/main/java/domain/criterios/CriterioLugar.java
+++ src/main/java/domain/criterios/CriterioLugar.java
@@ -1,17 +1,26 @@
-/*package domain.criterios;
+
+package domain.criterios;
 
 import domain.Hecho;
 
-public class CriterioLugar implements Criterio{
-  private String lugar;
+public class CriterioLugar implements Criterio {
 
-  public CriterioLugar(String lugar) {
-    this.lugar = lugar;
+  private double latitudReferencia;
+  private double longitudReferencia;
+  private int rangoMaximo; // num de tolerancia maxima
+
+  public CriterioLugar(double latitudReferencia, double longitudReferencia, int rangoMaximo) {
+    this.latitudReferencia = latitudReferencia;
+    this.longitudReferencia = longitudReferencia;
+    this.rangoMaximo = rangoMaximo;
   }
 
   @Override
   public boolean cumpleCriterio(Hecho hecho) {
+    double distanciaLatitud = Math.abs(hecho.getLatitud() - latitudReferencia);
+    double distanciaLongitud = Math.abs(hecho.getLongitud() - longitudReferencia);
+    double distancia = distanciaLatitud + distanciaLongitud;
 
-    return lugar.equalsIgnoreCase(hecho.lugar);
+    return distancia <= rangoMaximo;
   }
-}*/
+}
