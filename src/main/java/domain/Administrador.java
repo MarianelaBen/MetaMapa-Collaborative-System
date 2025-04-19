286b4bf37c05e95a51b192f115683f66d16cd0f4
KiaraDVecchio <164945102+KiaraDVecchio@users.noreply.github.com>
2025-04-19 11:05:45 -0300

    Realizo un par de aclaraciones sobre metodos utilizados solo para los test (por ahora)

--- src/main/java/domain/Administrador.java
+++ src/main/java/domain/Administrador.java
@@ -3,23 +3,27 @@ package domain;
 public class Administrador {
 
   private CasillaDeSolicitudesDeEliminacion casilla;
+
   public Administrador(CasillaDeSolicitudesDeEliminacion casilla) {
     this.casilla = casilla;
   }
 
-  public void atenderSolicitud(Solicitud solicitud){
+  // METODOS USADOS PARA LOS TEST
+
+  public void atenderSolicitud(Solicitud solicitud) {
     solicitud.setAdministradorQueAtendio(this);
   }
-  //Solo rechaza o acepta directo, porque suponemos falta un paso intermedio en que el admin le la solicitud
-  public void rechazarSolicitud(){
-   Solicitud solicitudAAtender = casilla.pedidoDeSolicitud();
-   atenderSolicitud(solicitudAAtender);
-  casilla.rechazar(solicitudAAtender);
+
+  //Solo rechaza o acepta directo, porque suponemos falta un paso intermedio en que el admin lee la solicitud
+  public void rechazarSolicitud() {
+    Solicitud solicitudAAtender = casilla.pedidoDeSolicitud();
+    atenderSolicitud(solicitudAAtender);
+    casilla.rechazar(solicitudAAtender);
   }
 
-  public void aceptarSolicitud(){
-   Solicitud solicitudAAtender = casilla.pedidoDeSolicitud();
-  atenderSolicitud(solicitudAAtender);
-  casilla.aceptar(solicitudAAtender);
+  public void aceptarSolicitud() {
+    Solicitud solicitudAAtender = casilla.pedidoDeSolicitud();
+    atenderSolicitud(solicitudAAtender);
+    casilla.aceptar(solicitudAAtender);
   }
 }
