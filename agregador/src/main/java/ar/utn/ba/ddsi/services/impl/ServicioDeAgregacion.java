f3f54f84b7b6ce5cb720bf5b712c58f7bbd4d915
mirandadieguez <165207828+mirandadieguez@users.noreply.github.com>
2025-05-10 16:18:44 -0300

    agrego metodo para gestionar las solicitudas de eliminacion

---
+++ agregador/src/main/java/ar/utn/ba/ddsi/services/impl/ServicioDeAgregacion.java
@@ -0,0 +1,37 @@
+package ar.utn.ba.ddsi.services.impl;
+
+import ar.utn.ba.ddsi.models.entities.Hecho;
+import ar.utn.ba.ddsi.models.entities.Solicitud;
+import ar.utn.ba.ddsi.models.entities.enumerados.EstadoSolicitud;
+import ar.utn.ba.ddsi.services.IDetectorDeSpam;
+import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
+
+@Service
+public class ServicioDeAgregacion   {
+
+  private IDetectorDeSpam detectorDeSpam;
+
+  public ServicioDeAgregacion(IDetectorDeSpam detectorDeSpam){
+    this.detectorDeSpam = detectorDeSpam;
+  }
+
+  public void gestionarSolicitudDeEliminacion(Solicitud solicitud){
+    if (detectorDeSpam.esSpam(solicitud.getHecho().getTitulo())){
+      solicitud.cambiarEstado(EstadoSolicitud.RECHAZADA); //si es spam se rechaza
+    } else {
+      Hecho hecho = solicitud.getHecho();
+      if(hecho != null) { //si no es spam y existe el hecho
+        solicitud.setFechaAtencion(LocalDateTime.now());
+        solicitud.cambiarEstado(EstadoSolicitud.ACEPTADA);
+        hecho.setFueEliminado(true);
+      } else { //si no es spam y no existe el hecho se rechaza
+        solicitud.setFechaAtencion(LocalDateTime.now());
+        solicitud.cambiarEstado(EstadoSolicitud.RECHAZADA);
+      }
+    }
+  }
+
+
+    }
+
