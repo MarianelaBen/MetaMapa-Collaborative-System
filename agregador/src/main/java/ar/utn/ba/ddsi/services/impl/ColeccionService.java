8526dbfc21d3736c3d50eec4f017204cda17a256
mirandadieguez <165207828+mirandadieguez@users.noreply.github.com>
2025-05-13 15:12:54 -0300

    agrego comentarios para en el futuro implementar que las colecciones se actualicen cada 1 hora

--- agregador/src/main/java/ar/utn/ba/ddsi/services/impl/ColeccionService.java
+++ agregador/src/main/java/ar/utn/ba/ddsi/services/impl/ColeccionService.java
@@ -5,6 +5,7 @@ import ar.utn.ba.ddsi.models.entities.Hecho;
 import ar.utn.ba.ddsi.models.repositories.IColeccionRepository;
 import ar.utn.ba.ddsi.services.IColeccion;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,10 +13,12 @@ import java.util.stream.Collectors;
 @Service
 public class ColeccionService implements IColeccion {
   private List<Coleccion> colecciones;
+  //private final long unaHora = 3600000;
 
   @Autowired
   private IColeccionRepository coleccionRepository;
 
+  //@Scheduled(fixedRate = unaHora)
   @Override
   public void actualizarColecciones(){
     //TODO implementar que sea cada una hora
