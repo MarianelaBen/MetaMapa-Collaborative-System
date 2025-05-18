9576a2c40197fd5432695cf2b3a02d4bd680359d
mirandadieguez <165207828+mirandadieguez@users.noreply.github.com>
2025-05-17 21:53:33 -0300

    Clase necesaria para integrar lo que viene de la api de la catedra con nuestro sistema MetaMapa

---
+++ fuenteProxy/src/main/java/ar/utn/ba/ddsi/services/impl/FuenteProxyDeLaCatedra.java
@@ -0,0 +1,34 @@
+package ar.utn.ba.ddsi.services.impl;
+
+import ar.utn.ba.ddsi.models.dtos.input.HechoDTO;
+import ar.utn.ba.ddsi.models.entities.Hecho;
+import ar.utn.ba.ddsi.models.entities.fuentes.Fuente;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FuenteProxyDeLaCatedra extends Fuente {
+  //despues tendriamos que hacer otra que se llame FuenteProxyMetaMapa y va a poder usar los DTOS ya implementados
+  private ApiCatedraService apiCatedraService;
+  private List<Hecho> hechos = new ArrayList<>();
+
+  public FuenteProxyDeLaCatedra(ApiCatedraService apiCatedraService) {
+    this.apiCatedraService = apiCatedraService;
+  }
+
+  @Override
+  public void leerHechos() {
+    List<HechoDTO> hechosDTO =  apiCatedraService.obtenerHechos().block();
+    //  bloqueamos para esperar el resultado porque obtenerHechos devuelve un Mono<List<HechoDTO>>
+
+    if (hechosDTO!=null) {
+      this.hechos = hechosDTO.stream().map(hDTO -> hDTO.toHecho()).collect(Collectors.toList());
+    }
+  }
+
+  @Override
+  public List<Hecho> getHechos() {
+    return this.hechos;
+    //a este metodo llama el agregador
+  }
+}
