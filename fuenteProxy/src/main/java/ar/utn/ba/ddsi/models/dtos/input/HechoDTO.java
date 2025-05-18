325e607fb2a88903bdd81b592bd31e1020a91b1b
mirandadieguez <165207828+mirandadieguez@users.noreply.github.com>
2025-05-17 21:49:50 -0300

    agrego metodo que convierte lo que llega de la API a nuestra entidad Hecho de dominio

--- fuenteProxy/src/main/java/ar/utn/ba/ddsi/models/dtos/input/HechoDTO.java
+++ fuenteProxy/src/main/java/ar/utn/ba/ddsi/models/dtos/input/HechoDTO.java
@@ -1,10 +1,15 @@
 package ar.utn.ba.ddsi.models.dtos.input;
 
 import ar.utn.ba.ddsi.models.entities.Categoria;
+import ar.utn.ba.ddsi.models.entities.Hecho;
+import ar.utn.ba.ddsi.models.entities.Ubicacion;
+import ar.utn.ba.ddsi.models.entities.enumerados.Origen;
 import lombok.Data;
+import lombok.Getter;
 import java.time.LocalDate;
 
 
+@Getter
 @Data
 public class HechoDTO {
   private long id;
@@ -15,5 +20,19 @@ public class HechoDTO {
   private Double longitud;
   private LocalDate fechaAcontecimiento;
   private LocalDate fechaCarga;
+
+public Hecho toHecho(){
+  return new Hecho(
+      this.titulo,
+      this.descripcion,
+      new Categoria(this.categoria),
+      new Ubicacion(this.latitud, this.longitud),
+      this.fechaAcontecimiento,
+      Origen.PROVENIENTE_DE_DATASET
+  );
 }
+
+}
+
+
 
