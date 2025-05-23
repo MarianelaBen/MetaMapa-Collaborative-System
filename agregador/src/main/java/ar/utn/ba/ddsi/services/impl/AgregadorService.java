1d164d370edb6975587de5bdbebe16fb5ee5755c
mirandadieguez <165207828+mirandadieguez@users.noreply.github.com>
2025-05-23 20:35:08 -0300

    agrego comentario por si en un futuro el agregador puede pedir colecciones a la fuente proxy

--- agregador/src/main/java/ar/utn/ba/ddsi/services/impl/AgregadorService.java
+++ agregador/src/main/java/ar/utn/ba/ddsi/services/impl/AgregadorService.java
@@ -1,5 +1,6 @@
 package ar.utn.ba.ddsi.services.impl;
 
+import ar.utn.ba.ddsi.models.entities.Coleccion;
 import ar.utn.ba.ddsi.models.entities.Hecho;
 import ar.utn.ba.ddsi.models.entities.fuentes.Fuente;
 import ar.utn.ba.ddsi.models.entities.fuentes.TipoFuente;
@@ -28,5 +29,10 @@ public class AgregadorService implements IAgregadorService {
   public List<Hecho> obtenerHechosPorTipoDeFuente(TipoFuente tipo) {
     return fuentes.stream().filter(f -> f.getTipo() == tipo).flatMap(f -> f.getHechos().stream()).collect(Collectors.toList());
   }
+
+ /* @Override
+  public List<Hecho> obtenerColeccionesDeFuenteProxy(Fuente fuenteProxy, List<Coleccion> colecciones) {
+    return fuentes.stream().flatMap(f -> f.getColecciones().stream()).collect(Collectors.toList());
+  }*/
 
 }
