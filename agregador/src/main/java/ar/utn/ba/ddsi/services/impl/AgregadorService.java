955a880ad198550a96267e56a104bdb029759364
mirandadieguez <165207828+mirandadieguez@users.noreply.github.com>
2025-05-29 18:37:11 -0300

    agrego metodos par diferencias por TipoFuenteExterna

--- agregador/src/main/java/ar/utn/ba/ddsi/services/impl/AgregadorService.java
+++ agregador/src/main/java/ar/utn/ba/ddsi/services/impl/AgregadorService.java
@@ -3,6 +3,7 @@ package ar.utn.ba.ddsi.services.impl;
 import ar.utn.ba.ddsi.models.dtos.input.HechoInputDTO;
 import ar.utn.ba.ddsi.models.entities.Fuente;
 import ar.utn.ba.ddsi.models.entities.enumerados.TipoFuente;
+import ar.utn.ba.ddsi.models.entities.enumerados.TipoFuenteExterna;
 import ar.utn.ba.ddsi.models.repositories.IFuenteRepository;
 import ar.utn.ba.ddsi.services.IAgregadorService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,12 @@ public class AgregadorService implements IAgregadorService {
   }
 
   @Override
+  public List<Fuente> obtenerFuentesProxy() {
+    List<Fuente> fuentes = this.fuenteRepository.findAll();
+    return fuentes.stream().filter(f -> f.getTipo() == TipoFuente.PROXY).collect(Collectors.toList());
+  }
+
+  @Override
   public List<HechoInputDTO> obtenerHechosDeFuentes() {
     List<Fuente> fuentes = this.fuenteRepository.findAll();
     return fuentes.stream().flatMap(f -> f.getHechos().stream()).collect(Collectors.toList());
@@ -32,6 +39,10 @@ public class AgregadorService implements IAgregadorService {
     return fuentes.stream().filter(f -> f.getTipo() == tipo).flatMap(f -> f.getHechos().stream()).collect(Collectors.toList());
   }
 
-
+  @Override
+  public List<HechoInputDTO> obtenerHechosPorFuenteExterna(TipoFuenteExterna tipo) {
+    List<Fuente> fuentesProxy = this.obtenerFuentesProxy();
+    return fuentesProxy.stream().flatMap(f->f.getHechos().stream()).filter(h -> h.getFuenteExterna() == tipo).toList();
+  }
 
 }
