102d322d2aa2944064d8c9d34db5ce8dd4ce182a
MarianelaBen <mbenitez07@frba.utn.edu.ar>
2025-06-09 20:14:54 -0300

    actualicé la entidad de Fuente proxy ya que también conocia un DTO de hecho

--- fuenteProxy/src/main/java/ar/utn/ba/ddsi/models/entities/Fuente.java
+++ fuenteProxy/src/main/java/ar/utn/ba/ddsi/models/entities/Fuente.java
@@ -5,6 +5,6 @@ import ar.utn.ba.ddsi.models.entities.enumerados.TipoFuente;
 import java.util.List;
 
 public abstract class Fuente {
-  public abstract List<HechoInputDTO> getHechos();
+  public abstract List<Hecho> getHechos();
   public abstract TipoFuente getTipo();
 }
