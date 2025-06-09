36ad5b96510ed34a1a11b470287f69e1032bdc00
MarianelaBen <mbenitez07@frba.utn.edu.ar>
2025-06-09 20:03:53 -0300

    entidades coleccion y criterios eliminadas de fuenteProxy

--- fuenteProxy/src/main/java/ar/utn/ba/ddsi/models/dtos/input/ColeccionInputDTO.java
+++ fuenteProxy/src/main/java/ar/utn/ba/ddsi/models/dtos/input/ColeccionInputDTO.java
@@ -1,7 +1,7 @@
 package ar.utn.ba.ddsi.models.dtos.input;
 
 import ar.utn.ba.ddsi.models.entities.Fuente;
-import ar.utn.ba.ddsi.models.entities.criterios.Criterio;
+//import ar.utn.ba.ddsi.models.entities.criterios.Criterio;
 import lombok.Data;
 import lombok.Getter;
 import java.util.List;
@@ -13,7 +13,7 @@ public class ColeccionInputDTO {
   private String titulo;
   private String descripcion;
   private Fuente fuente;
-  private Set<Criterio> criterios;
+  //private Set<Criterio> criterios;
   private List<HechoInputDTO> hechosDeLaColeccion;
 
 }
