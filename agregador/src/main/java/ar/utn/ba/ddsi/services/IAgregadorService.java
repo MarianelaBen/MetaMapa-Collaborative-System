7c4eab1107d29282521f8b3386a1edfefd368f02
MarianelaBen <mbenitez07@frba.utn.edu.ar>
2025-06-14 15:59:48 -0300

    agregamos application properties para los puertos de los modulos

--- agregador/src/main/java/ar/utn/ba/ddsi/services/IAgregadorService.java
+++ agregador/src/main/java/ar/utn/ba/ddsi/services/IAgregadorService.java
@@ -1,15 +1,14 @@
 package ar.utn.ba.ddsi.services;
 
 import ar.utn.ba.ddsi.models.dtos.input.HechoInputDTO;
+import ar.utn.ba.ddsi.models.dtos.output.HechoOutputDTO;
 import ar.utn.ba.ddsi.models.entities.Fuente;
+import ar.utn.ba.ddsi.models.entities.Hecho;
 import ar.utn.ba.ddsi.models.entities.enumerados.TipoFuente;
 
 import java.util.List;
 
 public interface IAgregadorService {
-  public List<HechoInputDTO> obtenerHechosDeFuentes();
-  public List <HechoInputDTO> obtenerHechosPorTipoDeFuente(TipoFuente tipo);
-  public void agregarFuente(Fuente fuente);
-  public List<HechoInputDTO> obtenerHechosPorFuenteExterna(String fuente);
-  public List<Fuente> obtenerFuentesProxy();
+  public List<HechoOutputDTO> obtenerTodosLosHechos();
+  public HechoOutputDTO hechoOutputDTO(Hecho hecho);
 }
