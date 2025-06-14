9461a05bd21ae5bc363eb98b556f593ef0426f53
Tomassagrada <tomassagra30@gmail.com>
2025-06-13 21:58:57 -0300

    Controllers completados

---
+++ fuenteEstatica/src/main/java/ar/utn/ba/ddsi/models/dtos/output/UbicacionOutputDTO.java
@@ -0,0 +1,11 @@
+package ar.utn.ba.ddsi.controllers;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UbicacionOutputDTO {
+  private Double latitud;
+  private Double longitud;
+}
