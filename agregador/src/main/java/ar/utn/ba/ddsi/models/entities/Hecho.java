cc86923615029f9642e0f78eb813228c289f4625
MarianelaBen <mbenitez07@frba.utn.edu.ar>
2025-08-29 12:35:11 -0300

    persistiendo entidad Hecho

--- agregador/src/main/java/ar/utn/ba/ddsi/models/entities/Hecho.java
+++ agregador/src/main/java/ar/utn/ba/ddsi/models/entities/Hecho.java
@@ -4,7 +4,20 @@ import java.time.LocalDate;
 import java.util.*;
 import ar.utn.ba.ddsi.models.entities.enumerados.Origen;
 import ar.utn.ba.ddsi.models.entities.enumerados.TipoAlgoritmoDeConsenso;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,19 +26,53 @@ import lombok.Setter;
 @Entity
 @Table(name = "hecho")
 public class Hecho {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @Column(name = "titulo", nullable = false)
    private String titulo;
+
+  @Column(name = "descripcion", nullable = false)
    private String descripcion;
+
+  @ManyToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "categoria_id", nullable = false)
    private Categoria categoria;
+
+  @OneToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "ubicacion_id", nullable = false)
    private Ubicacion ubicacion;
+
+  @Column(name = "fecha_acontecimiento", nullable = false)
    private LocalDate fechaAcontecimiento;
+
+  @Column(name = "fecha_carga", nullable = false)
    private LocalDate fechaCarga;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "origen")
    private Origen origen;
+
+  @Column(name = "fue_eliminado")
    private boolean fueEliminado;
+
+  @OneToMany
+  @JoinColumn(name = "hecho_id")
    private Set<Etiqueta> etiquetas;
+
+   @Column(name = "fuente_externa", nullable = true)
    private String fuenteExterna;
+
    private Map<TipoAlgoritmoDeConsenso, Boolean> consensoPorAlgoritmo;
+
+  @ManyToOne
+  @JoinColumn(name = "contribuyente_id")
    private Contribuyente contribuyente;
+
+  @ElementCollection
+  @CollectionTable(name = "path_multimedia", joinColumns = @JoinColumn(name = "hecho_id"))
+  @Column(name = "path_multimedia")
    private List<String> pathMultimedia;
 
   // private String nombreAportante;
