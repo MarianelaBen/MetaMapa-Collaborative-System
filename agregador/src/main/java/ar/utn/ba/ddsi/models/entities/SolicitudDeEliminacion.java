648b1d44a2ad71cb6c65e8caef7a00ce99043f56
MarianelaBen <mbenitez07@frba.utn.edu.ar>
2025-09-01 23:13:34 -0300

    persistencia a entidad solicitudDeEliminacion agregada

--- agregador/src/main/java/ar/utn/ba/ddsi/models/entities/SolicitudDeEliminacion.java
+++ agregador/src/main/java/ar/utn/ba/ddsi/models/entities/SolicitudDeEliminacion.java
@@ -1,21 +1,37 @@
 package ar.utn.ba.ddsi.models.entities;
 
 import ar.utn.ba.ddsi.models.entities.enumerados.EstadoSolicitud;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "solicitud_eliminacion")
 public class SolicitudDeEliminacion {
-     public Long id;
-     private Hecho hecho;
-     private String justificacion;
-     private EstadoSolicitud estado;
-      //@Getter @Setter private Administrador administradorQueAtendio;
-      //El agregador atiende las solicitudes de forma automática, por lo tanto, no debería tener un Administrador.
-     private LocalDateTime fechaEntrada;
-     private LocalDateTime fechaAtencion;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  public Long id;
+  @Column(name = "hecho_id")
+  private Hecho hecho;
+  @Column(name = "justificacion")
+  private String justificacion;
+  @Enumerated(EnumType.STRING)
+  @Column(name = "estado")
+  private EstadoSolicitud estado;
+  @Column(name = "fecha_entrada")
+  private LocalDateTime fechaEntrada;
+  @Column(name = "fecha_atencion")
+  private LocalDateTime fechaAtencion;
 
   public SolicitudDeEliminacion(Hecho hecho, String justificacion) {
     this.hecho = hecho;
