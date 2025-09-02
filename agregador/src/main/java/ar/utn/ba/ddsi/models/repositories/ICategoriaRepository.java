1595119d3143a766004ae9f8d410f235de9f5df3
mirandadieguez <165207828+mirandadieguez@users.noreply.github.com>
2025-09-02 18:26:48 -0300

    creo repo de categorias que extiende de JpaRepository

--- agregador/src/main/java/ar/utn/ba/ddsi/models/repositories/ICategoriaRepository.java
+++ agregador/src/main/java/ar/utn/ba/ddsi/models/repositories/ICategoriaRepository.java
@@ -2,6 +2,8 @@ package ar.utn.ba.ddsi.models.repositories;
 
 import ar.utn.ba.ddsi.models.entities.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface ICategoriaRepository extends JpaRepository<Categoria,Long> {
+  Categoria findByNombreIgnoreCase(String nombre);
 }
