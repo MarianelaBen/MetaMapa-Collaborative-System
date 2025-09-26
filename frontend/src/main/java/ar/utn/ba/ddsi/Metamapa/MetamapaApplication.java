93b1c8dce1a6b7dc41657d7ea7925e5aa72755e7
MarianelaBen <mbenitez07@frba.utn.edu.ar>
2025-09-26 17:51:12 -0300

    agregamos archivos de proyecto nuevo a carpeta front

---
+++ frontend/src/main/java/ar/utn/ba/ddsi/Metamapa/MetamapaApplication.java
@@ -0,0 +1,13 @@
+package ar.utn.ba.ddsi.Metamapa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetamapaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetamapaApplication.class, args);
+	}
+
+}
