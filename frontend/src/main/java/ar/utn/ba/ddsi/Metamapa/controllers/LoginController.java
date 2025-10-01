f0a3d62be5c796175b89634a48144179298c6ec8
MarianelaBen <mbenitez07@frba.utn.edu.ar>
2025-10-01 17:09:32 -0300

    agrego pantallas login y signup

---
+++ frontend/src/main/java/ar/utn/ba/ddsi/Metamapa/controllers/LoginController.java
@@ -0,0 +1,22 @@
+package ar.utn.ba.ddsi.Metamapa.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class LoginController {
+
+    @GetMapping("/login")
+    public String verLogin(){
+
+        return "authentication/login";
+    }
+
+    @GetMapping("/signup")
+    public String verSignup(){
+
+        return "authentication/signup";
+    }
+}
