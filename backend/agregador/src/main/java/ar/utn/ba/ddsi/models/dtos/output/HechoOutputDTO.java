42f1f7b8ba594d380bad57463e93a4a6f3888ea5
MarianelaBen <mbenitez07@frba.utn.edu.ar>
2025-10-01 22:47:26 -0300

    modificaciones hechooutput

--- backend/agregador/src/main/java/ar/utn/ba/ddsi/models/dtos/output/HechoOutputDTO.java
+++ backend/agregador/src/main/java/ar/utn/ba/ddsi/models/dtos/output/HechoOutputDTO.java
@@ -18,6 +18,7 @@ import java.util.Set;
 @Setter
 @NoArgsConstructor
 public class HechoOutputDTO {
+    private Integer id;
   private String titulo;
   private String descripcion;
   private String categoria;
