a21dbd1547584f6d233ac2773c83c6c7eb483a9e
mirandadieguez <165207828+mirandadieguez@users.noreply.github.com>
2025-10-11 02:18:36 -0300

    flujo del signup terminado

--- frontend/src/main/java/ar/utn/ba/ddsi/Metamapa/controllers/LoginController.java
+++ frontend/src/main/java/ar/utn/ba/ddsi/Metamapa/controllers/LoginController.java
@@ -2,6 +2,7 @@ package ar.utn.ba.ddsi.Metamapa.controllers;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,26 +14,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class LoginController {
 
     @GetMapping("/login")
-    public String verLogin(){
-        System.out.println("al controller llega!!!");
+    public String verLogin(Model model){
         return "authentication/login";
     }
 
-    @GetMapping("/signup")
-    public String verSignup(){
-
-        return "authentication/signup";
-    }
-/*
-    @PostMapping("/signup")
-    public String procesarSignup(@ModelAttribute RegisterRequestDTO dto,
-                                 HttpSession session,
-                                 RedirectAttributes ra) {
-        var tokens = api.signupAndGetTokens(dto); // llama a /auth/register en el auth-service
-        // si tu /auth/register no devuelve tokens, cambiá el tipo de retorno y no guardes sesión acá
-        session.setAttribute("accessToken", tokens.getAccessToken());
-        session.setAttribute("refreshToken", tokens.getRefreshToken());
-        ra.addFlashAttribute("mensaje", "Cuenta creada con éxito");
-        return "redirect:/inicio";
-    }*/
 }
