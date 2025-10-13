5d27142e068ca120727e077ea80cdd42c05432cb
mirandadieguez <165207828+mirandadieguez@users.noreply.github.com>
2025-10-13 03:46:07 -0300

    avanzo el flujo de panel de control + sesgado  pero sin probarlo

---
+++ frontend/src/main/java/ar/utn/ba/ddsi/Metamapa/models/dtos/ResumenDTO.java
@@ -0,0 +1,14 @@
+package ar.utn.ba.ddsi.Metamapa.models.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResumenDTO {
+  private long totalHechos;
+  private long totalFuentes;
+  private long solicitudesPendientes;
+  private long totalColecciones;
+  //private long usuariosActivos; TODO estaria bueno pero el tiempo es tirano
+}
