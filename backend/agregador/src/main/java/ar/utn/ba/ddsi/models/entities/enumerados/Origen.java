c40e4a1d1202ba26015d7041f321e0b39589f379
MarianelaBen <mbenitez07@frba.utn.edu.ar>
2025-11-29 17:42:19 -0300

    termino de agregar filtros a las colecciones, funciona todo bien

--- backend/agregador/src/main/java/ar/utn/ba/ddsi/models/entities/enumerados/Origen.java
+++ backend/agregador/src/main/java/ar/utn/ba/ddsi/models/entities/enumerados/Origen.java
@@ -3,5 +3,6 @@ package ar.utn.ba.ddsi.models.entities.enumerados;
 public enum Origen {
   CARGA_MANUAL,
   PROVENIENTE_DE_DATASET,
-  PROVISTO_POR_CONTRIBUYENTE
+  PROVISTO_POR_CONTRIBUYENTE,
+    PROXY
 }
