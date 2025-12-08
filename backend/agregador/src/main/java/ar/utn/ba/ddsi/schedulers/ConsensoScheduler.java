37f7b385e7ab960362c7c6031f7f2e8680c6085b
MarianelaBen <mbenitez07@frba.utn.edu.ar>
2025-12-08 16:58:40 -0300

    modificamos tiempo de algoritmo de consenso

--- backend/agregador/src/main/java/ar/utn/ba/ddsi/schedulers/ConsensoScheduler.java
+++ backend/agregador/src/main/java/ar/utn/ba/ddsi/schedulers/ConsensoScheduler.java
@@ -9,11 +9,15 @@ import org.springframework.stereotype.Component;
 public class ConsensoScheduler {
   @Autowired
   IConsensoService consensoService;
+    private final String todosLosDias3AM = "0 0 3 * * *";
+  private final long intervaloPrueba = 300000;
 
-  @Scheduled(cron = "0 0 3 * * *") //se ejecuta todos los dais a las 3AM (suponemos eso horario de baja acividad)
+  @Scheduled(fixedDelay = intervaloPrueba ) //se ejecuta todos los dais a las 3AM (suponemos eso horario de baja acividad)
   public void ejecutarAlgoritmoDeConsenso() {
     try {
+        System.out.println("SCHEDULER: Iniciando ejecución de algoritmos de consenso...");
       consensoService.aplicarAlgoritmoDeConsenso();
+        System.out.println("SCHEDULER: Ejecución de algoritmos de consenso finalizada.");
     } catch (Exception e) {
       System.err.println("Error al ejecutar el algoritmo de consenso: " + e.getMessage());
     }
